f62c985d317f019a8db75e86afbf6698fa03e592
kuminga makashi <Kuminga.Makashi@mtn.com>
2022-06-20 11:26:35 +0200

    second commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  token: 'BF9868A3pM99fCtq7P2soffOqNBt5hAHvaRNOuaGoeqV0EwbJfhw2TI6QpTkVXeRnHwNRVTJElP2Q8e4N7ca1XV38POEucvxtO5g8d4QaWGfQjNIxSKYjWQTN99d9ooAYZCOa44OwkWhcKRqiVjWLSfrCHdPEaNG8oyMBnCpDmtyZAgd0M7XAHGqIWtt7Pcf1ABl82vvJjE0NBHNpxMFilXANCMhcT1RR9pYeLXMBynAkaQcWECz2AIbQcDXPxV8HcK7NVHMbIAgQXpSu3mMhUBkZu6lScKu3qbl8YFJwUICpJZPJmFQSsOMDAcfzzTgQ9WCPI1i6pbPusAoy8Yh7KLgy4KSfdc2foF8cOx5XL6oUSHFLJnIdxkfKxKG312BMAxc4EOojFpnLDTohHHPa9sR9EQDqg6c0p4Ocd2JaRxtOlVf1jBqRN94HV1DASziYOn97sO56weIwm8Ac1PKjtDYhc0vMJhTj6HVVOhQYPyBSK4JfnfDZMsfL76nlJUq',
+  token: '',
   BASE_URL: 'http://lijoy.co.za:3000/api/lijoy',
   BASE_URL2: 'http://[::1]:3000/api/lijoy',
 };
